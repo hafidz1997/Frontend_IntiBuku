3e9bc5d2603cd59e9b84f73b59867500e1c648a4
devyapriansyah <devyapriansyah@gmail.com>
2018-12-17 14:11:45 +0700

    gabung beranda dari hafidz

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,14 +3,30 @@ import { NavController } from 'ionic-angular';
 import { DetailPostingPage } from '../detail-posting/detail-posting';
 import { PostingPage } from '../posting/posting';
 
+import { HttpHeaders, HttpClient} from '@angular/common/http'
+import { Data } from '../../provider/data';
+
+
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,public httpClient: HttpClient, public data : Data) {
 
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer '+data.api_token
+      })
+    };
+
+    this.httpClient.get(this.data.BASE_URL+'/getReview'+ data, httpOptions).subscribe(data =>{
+      let response = data;
+      console.log(response);
+    });
   }
 
   detail_posting(){
